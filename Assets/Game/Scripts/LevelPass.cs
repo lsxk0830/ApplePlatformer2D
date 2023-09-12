333a61bb3cdcc6a0d3af88708c7cbbbc08fde09c
蓝色星空 <lsxk0830@163.com>
2023-09-12 22:10:08 +0800

    5.1.4 【游戏设计】目标

---
+++ Assets/Game/Scripts/LevelPass.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Blue
+{
+    public class LevelPass : MonoBehaviour
+    {
+        public Text LevelPassText;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var currentScene = SceneManager.GetActiveScene();
+
+            LevelPassText.gameObject.SetActive(true);
+
+            StartCoroutine(Delay(2,()=>
+            {
+                SceneManager.LoadScene(currentScene.name);
+            }));
+        }
+
+        private IEnumerator Delay(float seconds,Action onFinish)
+        {
+            yield return new WaitForSeconds(seconds);
+            onFinish?.Invoke();
+        }
+    }
+}
