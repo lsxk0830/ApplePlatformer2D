eaee43faf76b35bd9549d330f506942dd73da009
蓝色星空 <lsxk0830@163.com>
2023-09-12 16:41:08 +0800

    3.1.2 控制手感调整

--- Assets/Game/Scripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ namespace Blue
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody2D mRigidbody2D;
+        public float HorizontalMovementSpeed = 5; // 水平移动速度
+        public float JumpSpeed = 5; // 跳跃速度
+
+        public float JumpUpGravity = 3; // 跳跃时上升时重力
+        public float FallDownGravity = 6; // 跳跃时下落时重力
         private void Start()
         {
             mRigidbody2D = GetComponent<Rigidbody2D>();
@@ -14,12 +19,29 @@ namespace Blue
         {
             var horizontal = Input.GetAxis("Horizontal");
 
-            if (Input.GetKeyDown(KeyCode.K))
+            if (Input.GetKeyDown(KeyCode.K) && CollisionObjectCount > 0)
             {
-                mRigidbody2D.velocity = new Vector2(mRigidbody2D.velocity.x, 5); // 设置速度--垂直
+                mRigidbody2D.velocity = new Vector2(mRigidbody2D.velocity.x, JumpSpeed); // 设置速度--垂直
             }
 
-            mRigidbody2D.velocity = new Vector2(horizontal, mRigidbody2D.velocity.y); // 设置速度--水平
+            mRigidbody2D.velocity = new Vector2(horizontal * HorizontalMovementSpeed, mRigidbody2D.velocity.y); // 设置速度--水平
+
+            // 调整跳跃上升、下落重力
+            if (mRigidbody2D.velocity.y > 0)
+                mRigidbody2D.gravityScale = JumpUpGravity;
+            else
+                mRigidbody2D.gravityScale = FallDownGravity;
+        }
+
+        public int CollisionObjectCount;
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            CollisionObjectCount++;
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            CollisionObjectCount--;
         }
     }
 }
