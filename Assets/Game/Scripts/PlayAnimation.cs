cbc41f29e01d0942b7d56e95a375e709dc393559
蓝色星空 <lsxk0830@163.com>
2023-09-13 22:55:41 +0800

    11.1.10 【开发】增加主角的简单动画

---
+++ Assets/Game/Scripts/PlayAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Blue
+{
+    public class PlayAnimation : MonoBehaviour
+    {
+        private Rigidbody2D mRigidbody2D;
+        private PlayerMovement mPlayerMovement;
+        void Start()
+        {
+            mRigidbody2D = GetComponent<Rigidbody2D>();
+            mPlayerMovement = GetComponent<PlayerMovement>();
+        }
+
+
+        void Update()
+        {
+            var velocity = mRigidbody2D.velocity;
+
+            var scaleXOffset = 0.3f * Mathf.Abs(velocity.x / mPlayerMovement.HorizontalMovementSpeed);
+            var scale = new Vector3(1f + scaleXOffset, 0.3f + 0.7f, 1);
+
+            transform.localScale = scale;
+        }
+    }
+}
