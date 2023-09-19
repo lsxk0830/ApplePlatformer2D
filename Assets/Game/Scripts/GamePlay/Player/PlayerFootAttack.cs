4aea175bcad10c0de6a157fe62b9169590536ed8
蓝色星空 <lsxk0830@163.com>
2023-09-19 21:07:24 +0800

    7.3.3 第三关 增加敌人形象、跳踩反馈

--- Assets/Game/Scripts/GamePlay/Player/PlayerFootAttack.cs
+++ Assets/Game/Scripts/GamePlay/Player/PlayerFootAttack.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Blue
 {
+    /// <summary>
+    /// 玩家攻击检测
+    /// </summary>
     public class PlayerFootAttack : MonoBehaviour
     {
         public Trigger2D FootAttackCheck;
 
+        public float JumpSpeed = 15;
+        private Rigidbody2D mRigidbody2D;
+
+        public UnityEvent OnFootAttack = new UnityEvent();
         void Awake()
         {
+            mRigidbody2D = GetComponent<Rigidbody2D>();
+
             FootAttackCheck.OnTriggerEnterWithCollider.AddListener(collider=>
             {
                 collider.GetComponent<CharacterHit>().Hit();
+
+                mRigidbody2D.velocity = new Vector2(mRigidbody2D.velocity.x,JumpSpeed);
+
+                OnFootAttack?.Invoke();
             });
         }
     }
