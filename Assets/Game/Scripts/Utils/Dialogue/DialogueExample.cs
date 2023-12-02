7cea87066c71d90cedf12e9bac3cdd39e69136e7
BlueSky <lsxk0830@163.com>
2023-12-02 14:29:22 +0800

    8.10.8【开发】对话系统支持 Text 组件和 文字播放效果

--- Assets/Game/Scripts/Utils/Dialogue/DialogueExample.cs
+++ Assets/Game/Scripts/Utils/Dialogue/DialogueExample.cs
@@ -1,11 +1,42 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Blue
 {
     public class DialogueExample : MonoBehaviour
     {
+        /// <summary>
+        /// 对话播放状态
+        /// </summary>
+        public enum DialogueStates
+        {
+            /// <summary>
+            /// 未开始
+            /// </summary>
+            NotStart,
+            /// <summary>
+            /// 正在播放
+            /// </summary>
+            Started,
+            /// <summary>
+            /// 已完成
+            /// </summary>
+            Finished
+        }
+
+        public DialogueStates State = DialogueStates.NotStart;
         private Queue<string> mSentenceQueue = new Queue<string>();
+
+        // 状态事件回调
+        public UnityEvent OnDialogueNotStart = new UnityEvent();
+        public UnityEvent OnDialogueStart = new UnityEvent();
+        public UnityEvent OnDialogueFinish = new UnityEvent();
+        // 对外提供设置文本的方法
+        public UnityEvent<string> OnPlayText = new UnityEvent<string>();
+
         private void Awake()
         {
             mSentenceQueue.Enqueue("Hello World~");
@@ -13,15 +44,61 @@ namespace Blue
             mSentenceQueue.Enqueue("Welcome to my world~");
             mSentenceQueue.Enqueue("Happy learning~");
 
-            Debug.Log(mSentenceQueue.Dequeue());
+            OnDialogueNotStart?.Invoke();
         }
 
-        private void Update()
+        /// <summary>
+        /// 对外提供一个开始播放的方法
+        /// </summary>
+        public void StartDialogue()
         {
-            if (Input.GetMouseButtonDown(0) && mSentenceQueue.Count > 0)
+            if (State == DialogueStates.NotStart)
             {
-                Debug.Log(mSentenceQueue.Dequeue());
+                State = DialogueStates.Started;
+                OnDialogueStart?.Invoke();
+                Next();
             }
+        }
+
+        /// <summary>
+        /// 对外提供下一步方法
+        /// </summary>
+        public void Next()
+        {
+            if (State == DialogueStates.Started)
+            {
+                if (mSentenceQueue.Count > 0)
+                {
+                    StartCoroutine(StartPlayText(mSentenceQueue.Dequeue()));
+                }
+                else
+                {
+                    StartCoroutine(StartPlayText(string.Empty));
+                    State = DialogueStates.Finished;
+                    OnDialogueFinish?.Invoke();
+                }
+            }
+        }
+
+        private string mCurrentSentence = string.Empty;
+        /// <summary>
+        /// 文字播放效果
+        /// </summary>
+        /// <param name="Sentence">要播放的句子</param>
+        private IEnumerator StartPlayText(string Sentence)
+        {
+            mCurrentSentence = Sentence;
+            var sentenceToPlay = string.Empty;
+            var length = mCurrentSentence.Length;
+            for (int i = 0; i < length;i++)
+            {
+                yield return new WaitForSeconds(0.1f);
+                sentenceToPlay = mCurrentSentence.Substring(0,i);
+                OnPlayText?.Invoke(sentenceToPlay);
+            }
+            yield return new WaitForSeconds(0.1f);
+            sentenceToPlay = mCurrentSentence;
+            OnPlayText?.Invoke(sentenceToPlay);
         }
     }
 }
