14930fec6f2cddd2d5abc0d06fd825fc785c352f
BlueSky <lsxk0830@163.com>
2023-12-17 15:12:48 +0800

    5.13.5 【开发】冲刺次数限制&冲刺音效

--- Assets/Game/Scripts/GamePlay/Player/PlayerDash.cs
+++ Assets/Game/Scripts/GamePlay/Player/PlayerDash.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Blue
 {
@@ -9,6 +10,9 @@ namespace Blue
     {
         private Rigidbody2D mRigidbody2D;
         private DashRule mDashRule;
+
+        public UnityEvent OnDash = new UnityEvent();
+
         public float Speed = 20;
 
         /// <summary>
@@ -17,23 +21,40 @@ namespace Blue
         public float Duration = 0.5f;
 
         /// <summary>
+        /// 是否可冲刺
+        /// </summary>
+        private bool mCanDash = true;
+
+        /// <summary>
         /// 冲刺之前的重力数值
         /// </summary>
         private float mCachedGravityScale = 0;
+
+        public Trigger2D GroundCheck;
 
         private void Awake()
         {
             mRigidbody2D = GetComponent<Rigidbody2D>();
             mDashRule = ApplePlatformer2D.Interface.GetSystem<IBonfireSystem>().GetRuleByKey(nameof(DashRule)) as DashRule;
+
+            GroundCheck.OnTriggerEnter.AddListener(()=>
+            {
+                mCanDash = true; // 落地重置冲刺状态
+            });
         }
 
         private float mDashStartTime = 0f;
+
+        /// <summary>
+        /// 冲刺中
+        /// </summary>
         private bool Dashing = false;
+
         private void Update()
         {
             if (mDashRule.Unlocked)
             {
-                if (Input.GetKeyDown(KeyCode.O))
+                if (Input.GetKeyDown(KeyCode.O) && !Dashing && mCanDash)
                 {
                     mRigidbody2D.velocity = Vector2.right * Speed * Mathf.Sign(transform.localScale.x);
                     GetComponent<PlayerMovement>().enabled = false;
@@ -43,6 +64,9 @@ namespace Blue
 
                     mDashStartTime = Time.time;
                     Dashing = true;
+                    mCanDash = false;
+
+                    OnDash?.Invoke();
                 }
 
                 if (Dashing && mDashStartTime + Duration < Time.time)
@@ -50,6 +74,12 @@ namespace Blue
                     Dashing = false;
                     mRigidbody2D.gravityScale = mCachedGravityScale;
                     GetComponent<PlayerMovement>().enabled = true;
+
+                    // 冲刺结束时，如果是落地状态，则重置
+                    if(GroundCheck.Triggered)
+                    {
+                        mCanDash = true;
+                    }
                 }
             }
         }
